1d98f2260b3c8a8fceb0f13832bb45135c4874c9
ndrsh <>
2022-12-11 17:24:18 +0200

    Improve readability

--- src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day2.kt
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day2.kt
@@ -6,18 +6,28 @@ import java.io.File
 fun main(args: Array<String>) {
 	val lines = File("/home/ndrsh/software/adventofcode/2022/2").readLines()
 	
-	lines.sumOf {
-		val (him, me) = it.first().code - 'A'.code to it.last().code - 'X'.code
-		val score = if (him == me) 3 else if ((him + 1)%3 == me) 6 else 0
-		score + me + 1
-	}.let { println(it) }
+	// 0=Rock, 1=Paper, 2=Scissors
+	fun String.strategyGuide(): Pair<Int, Int> = first().code - 'A'.code to last().code - 'X'.code
 	
-	lines.sumOf {
-		val (first, second) = it.first().code - 'A'.code to it.last().code - 'X'.code
+	val ans1 = lines.sumOf {
+		val (him, me) = it.strategyGuide()
+		val score = when {
+			him == me         -> 3
+			(him + 1)%3 == me -> 6
+			else              -> 0
+		}
+		score + me + 1
+	}
+	
+	val ans2 = lines.sumOf {
+		val (first, second) = it.strategyGuide()
 		1 + when (second) {
 			0    -> (first + 2)%3
 			1    -> 3 + first
 			else -> 6 + (first + 1)%3
 		}
-	}.let { println(it) }
+	}
+	
+	println(ans1)
+	println(ans2)
 }
