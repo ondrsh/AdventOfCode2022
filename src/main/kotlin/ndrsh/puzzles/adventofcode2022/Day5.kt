71e148a574a58edf05f9cab402ee9742046e357a
ndrsh <>
2022-12-11 18:10:41 +0200

    Use Strings as stacks

--- src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day5.kt
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day5.kt
@@ -1,20 +1,28 @@
 package ndrsh.puzzles.adventofcode2022
 
 import ndrsh.puzzles.getAllInts
-import ndrsh.puzzles.removeLast
 import java.io.File
 
 fun main(args: Array<String>) {
 	val lines = File("/home/ndrsh/software/adventofcode/2022/5").readLines()
-	val stacks = lines.maxOf { it.length }.let { (0..it/4).map { mutableListOf<Char>() } }
+	val cols = lines.first().length
+	val stacks = MutableList(1 + cols/4) { "" }
 	
-	lines.takeWhile { it.isNotEmpty() }.reversed().drop(1).forEach {
-		it.forEachIndexed { i, c -> if (c.isUpperCase()) { stacks[i/4].add(c)} }
-	}
+	lines.takeWhile { it.isNotEmpty() }
+			.reversed()
+			.drop(1)
+			.forEach { line ->
+				line.forEachIndexed { i, char ->
+					if (char.isUpperCase()) stacks[i/4] += char.toString()
+				}
+			}
+	
 	lines.filter { it.contains("move") }.forEach {
 		val (n, from, to) = it.getAllInts()
-		stacks[to-1].addAll(stacks[from-1].removeLast(n).reversed()) // for part 2, drop .reversed()
+		stacks[to - 1] += stacks[from - 1].takeLast(n).reversed() // for part 2, drop .reversed()
+		stacks[from - 1] = stacks[from - 1].dropLast(n)
 	}
-	println(stacks.map { it.last() }.joinToString(""))
+	
+	val ans = stacks.map { it.last() }.joinToString("")
+	println(ans)
 }
-
