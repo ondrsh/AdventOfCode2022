882ae539dca78c07920bcb63b6bd2b2620ff77c4
ndrsh <>
2022-12-11 16:54:54 +0200

    Improve readability

--- src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day11.kt
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day11.kt
@@ -1,74 +1,73 @@
 package ndrsh.puzzles.adventofcode2022
 
-
 import ndrsh.puzzles.getAllLongs
 import java.io.File
 
 fun main(args: Array<String>) {
 	val lines = File("/home/ndrsh/software/adventofcode/2022/11").readLines()
-	lines.solve(part2 = false)
-	lines.solve(part2 = true)
+	lines.solve(part1 = true)
+	lines.solve(part1 = false)
 }
 
-private fun List<String>.solve(part2: Boolean) {
+data class Monkey(val items: MutableList<Long>,
+                  var nHandled: Long = 0,
+                  val op: (Long) -> Long,
+                  val test: (Long) -> Boolean,
+                  val ifTrue: Int,
+                  val ifFalse: Int)
+
+private fun List<String>.solve(part1: Boolean) {
+	val n = if (part1) 20 else 10000
+	val k = if (part1) 3 else 1
 	val monkeys = mutableListOf<Monkey>()
-	val mod = parseAndFill(monkeys)
+	val product = parseAndFill(monkeys)
 	
 	fun Monkey.handle() {
 		while (items.isNotEmpty()) {
 			val item = items.removeFirst()
 			var v = op(item)
-			v /= if (part2) 1 else 3
+			v /= k
 			val target = if (test(v)) monkeys[ifTrue] else monkeys[ifFalse]
-			target.items.add(v%mod)
-			handled++
+			target.items.add(v%product)
+			nHandled++
 		}
 	}
 	
-	repeat(if (part2) 10000 else 20) {
+	repeat(n) {
 		monkeys.forEach { it.handle() }
 	}
 	
-	val ans = monkeys.map { it.handled }.sortedDescending().take(2).reduce(Long::times)
+	val ans = monkeys.map { it.nHandled }
+			.sortedDescending()
+			.take(2)
+			.reduce(Long::times)
+	
 	println(ans)
 }
 
 private fun List<String>.parseAndFill(monkeys: MutableList<Monkey>): Long {
-	val allDivs = mutableListOf<Long>()
-	windowed(6, 7) {
-		val items = it[1].getAllLongs()
-		val opstr = it[2].substringAfter("new = old ")
-		val multiplies = opstr.contains('*')
+	var product = 1L
+	
+	windowed(size = 6, step = 7) { lines ->
+		val items = lines[1].getAllLongs().toMutableList()
+		val opstr = lines[2].substringAfter("new = old ")
+		val divBy = lines[3].split(" ").last().toLong()
+		val ifTrue = lines[4].split(" ").last().toInt()
+		val ifFalse = lines[5].split(" ").last().toInt()
+		product *= divBy
 		
-		val op: (Long) -> Long = {
-			if (opstr.contains("old")) {
-				if (multiplies) it*it
-				else it + it
-			} else {
-				opstr.split(" ").last().toLong().let { num ->
-					if (multiplies) it*num
-					else it + num
-				}
-			}
+		val op: (Long) -> Long = { a ->
+			val b = if (opstr.contains("old")) a else opstr.split(" ").last().toLong()
+			if (opstr.contains('*')) a*b
+			else a + b
 		}
 		
-		val ifTrue = it[4].split(" ").last().toInt()
-		val ifFalse = it[5].split(" ").last().toInt()
-		val divBy = it[3].split(" ").last().toLong()
-		allDivs.add(divBy)
-		
-		monkeys.add(Monkey(items = items.toMutableList(),
+		monkeys.add(Monkey(items = items,
 		                   op = op,
 		                   test = { it%divBy == 0L },
 		                   ifTrue = ifTrue,
 		                   ifFalse = ifFalse))
 	}
-	return allDivs.reduce(Long::times)
-}
 
-data class Monkey(val items: MutableList<Long>,
-                  var handled: Long = 0,
-                  val op: (Long) -> Long,
-                  val test: (Long) -> Boolean,
-                  val ifTrue: Int,
-                  val ifFalse: Int)
+	return product
+}
