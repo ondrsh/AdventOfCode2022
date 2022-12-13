54c6fb447ec7314512161e3925d38d930521a086
ndrsh <>
2022-12-13 15:30:25 +0200

    Inline properties

--- src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day13.kt
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day13.kt
@@ -3,15 +3,12 @@ package ndrsh.puzzles.adventofcode2022
 import java.io.File
 
 fun main(args: Array<String>) {
-	val lines = File("/home/ndrsh/software/adventofcode/2022/13").readLines()
-	val pairs = lines.windowed(2, 3)
-	
+	val pairs = File("/home/ndrsh/software/adventofcode/2022/13").readLines().windowed(2, 3)
 	val compare: (String, String) -> Int = { s1: String, s2: String -> if (inOrder(s1.toMutableList(), s2.toMutableList())) -1 else 1 }
 	val ans1 = pairs.mapIndexed { i, packets -> if (compare(packets.first(), packets.last()) != 1) i + 1 else 0 }.sum()
 	println(ans1)
 	
-	val packets = pairs.flatten() + "[[2]]" + "[[6]]"
-	val sorted = packets.sortedWith(compare)
+	val sorted = (pairs.flatten() + "[[2]]" + "[[6]]").sortedWith(compare)
 	val ans2 = (1 + sorted.indexOf("[[2]]"))*(1 + sorted.indexOf("[[6]]"))
 	println(ans2)
 }
