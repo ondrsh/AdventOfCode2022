d2b21e28de6453c3ef8f094c09f21a7d1ea4c31d
ndrsh <>
2022-12-14 10:17:05 +0200

    Solve day 14

---
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day14.kt
@@ -0,0 +1,45 @@
+package ndrsh.puzzles.adventofcode2022
+
+import java.io.File
+
+fun main(args: Array<String>) {
+	val lines = File("/home/ndrsh/software/adventofcode/2022/14").readLines()
+	val (sandStart, cols) = 500 to 700
+	var (ans1, ans2) = 0 to 0
+	
+	// mapping each point p to an integer k where p.x = k%cols and p.y = k/cols
+	val paths: List<List<Int>> = lines.map { it.split(" -> ").map { it.split(",").map { it.toInt() }.let { it.first() + cols*it.last() } } }
+	val rows = paths.flatten().max()/cols + 2
+	val points = BooleanArray((rows + 1)*(cols + 1))
+	
+	paths.forEach {
+		it.windowed(2).forEach {
+			val (l, h) = it.sorted()
+			val range = if (l%cols != h%cols) l..h else l..h step cols
+			range.forEach { points[it] = true }
+		}
+	}
+	
+	val last = points.indices.filter { points[it] }.max()
+	val bottom = (rows*cols until (rows + 1)*cols)
+	bottom.forEach { points[it] = true }
+	
+	while (true) {
+		var k = sandStart
+		while (k < cols*rows) {
+			k += cols + if (points[k + cols]) {
+				if (points[k + cols - 1]) {
+					if (points[k + cols + 1]) {
+						points[k] = true
+						break
+					} else 1
+				} else -1
+			} else 0
+		}
+		if (k > last && ans1 == 0) ans1 = ans2
+		if (points[sandStart]) break
+		ans2++
+	}
+	println(ans1)
+	println(++ans2)
+}
