770a9f5e783a96f4b58bd30eb997d2d3e2bfe477
ndrsh <>
2022-12-24 18:07:19 +0200

    Simplify

--- src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day24.kt
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day24.kt
@@ -12,14 +12,14 @@ fun main(args: Array<String>) {
     val lcm = lcm(rs.toLong(), cs.toLong()).toInt()
     val freeStates = Array(lcm) { Array(rs) { BooleanArray(cs) { true } } }
     
-    data class Element(val p: Point, val minutes: Int)
-    data class Situation(val p: Point, val cycle: Int)
+    data class Situation(val p: Point, val minute: Int)
     data class Blizzard(val p: Point, val dir: Char) : Location by p {
         fun move() = copy(p = when (dir) {
-                                'v'  -> p.down().mod(rs, cs)
-                                '^'  -> p.up().mod(rs, cs)
-                                '>'  -> p.right().mod(rs, cs)
-                                else -> p.left().mod(rs, cs) })
+            'v'  -> p.down().mod(rs, cs)
+            '^'  -> p.up().mod(rs, cs)
+            '>'  -> p.right().mod(rs, cs)
+            else -> p.left().mod(rs, cs)
+        })
     }
     
     val blizzards = lines.drop(1).dropLast(1).flatMapIndexed { m, line ->
@@ -31,28 +31,25 @@ fun main(args: Array<String>) {
     for (i in 0 until lcm) {
         for (j in blizzards.indices) {
             val b = blizzards.removeAt(j)
-            freeStates[i][b] = false
+            freeStates[i][b.m][b.n] = false
             blizzards.add(j, b.move())
         }
     }
     
     fun solve(start: Point, end: Point, minutes: Int = 0): Int {
-        val queue = ArrayDeque<Element>()
-        queue.add(Element(start, minutes))
+        val queue = mutableListOf(Situation(start, minutes))
         val best = mutableMapOf<Situation, Int>()
         while (true) {
             val e = queue.removeFirst()
-            if (e.p == end) return e.minutes
-            val situation = Situation(e.p, e.minutes.mod(lcm))
-            if (e.minutes >= (best[situation] ?: Int.MAX_VALUE)) {
-                continue
-            }
-            best[situation] = e.minutes
-            val state = freeStates[(e.minutes + 1).mod(lcm)]
-            if (e.p == start || state[e.p]) queue.add(e.copy(minutes = e.minutes + 1))
+            if (e.p == end) return e.minute
+            val s = Situation(e.p, e.minute.mod(lcm))
+            if (e.minute >= (best[s] ?: Int.MAX_VALUE)) continue
+            else best[s] = e.minute
+            val state = freeStates[(e.minute + 1).mod(lcm)]
+            if (e.p == start || state[e.p]) queue.add(e.copy(minute = e.minute + 1))
             e.p.cardinalAdjacents().filter { it == end || state[it] }.forEach {
-                queue.add(Element(p = it,
-                                  minutes = e.minutes + 1))
+                queue.add(Situation(p = it,
+                                    minute = e.minute + 1))
             }
         }
     }
@@ -67,6 +64,4 @@ fun main(args: Array<String>) {
 
 operator fun Array<BooleanArray>.contains(loc: Location) = loc.m in indices && loc.n in 0 until first().size
 operator fun Array<BooleanArray>.get(loc: Location) = if (loc !in this) false else get(loc.m)[loc.n]
-operator fun Array<BooleanArray>.set(loc: Location, value: Boolean) {
-    get(loc.m)[loc.n] = value
-}
+operator fun Array<BooleanArray>.set(loc: Location, value: Boolean) { get(loc.m)[loc.n] = value }
