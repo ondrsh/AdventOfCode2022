1d23455b81da738bcba9cef8f86f088ac81c3a10
ndrsh <>
2022-12-26 20:12:59 +0200

    Simplify

--- src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day25.kt
+++ src/main/kotlin/ndrsh/puzzles/adventofcode2022/Day25.kt
@@ -3,7 +3,6 @@ package ndrsh.puzzles.adventofcode2022
 import java.io.File
 
 // solving runs in 3µs but could be reduced to 1.6µs when using when-statement in sumOf()
-
 fun main(args: Array<String>) {
     val lines = File("/home/ndrsh/software/adventofcode/2022/25").readLines().map { it.reversed() }
     val len = lines.maxOf { it.length }
@@ -12,9 +11,8 @@ fun main(args: Array<String>) {
     var carry = 0
     var ans1 = ""
     while (carry != 0 || k < len) {
-        var v = carry + lines.sumOf { if (it.length <= k) 0 else digits.indexOf(it[k]) - 2 }
+        val v = carry + lines.sumOf { if (it.length <= k) 0 else digits.indexOf(it[k]) - 2 }
         carry = (v + 2)/5
-        v %= 5
         ans1 = digits[(v + 2)%5] + ans1
         k++
     }
